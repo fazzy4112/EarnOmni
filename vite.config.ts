2ff239d890db70077c82927ec09e615751d675fd
fazzy4112 <techwithfazzy@gmail.com>
2026-07-03 03:31:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import viteTs from "vite-tsconfig-paths"
+import tailwindcss from "tailwindcss"
 
 export default defineConfig({
   plugins: [
@@ -9,4 +10,9 @@ export default defineConfig({
     react(),
     viteTs(),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 })
